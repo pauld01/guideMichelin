a4fe3748cada3a5e7311ab9a6bdc9bcafe08cca2
Paul D <paul.duplessi@hotmail.fr>
2021-11-12 21:37:06 +0100

    add accueil.html.twig update GuideController.php add menu.html.twig add routes.yaml

--- src/Controller/GuideController.php
+++ src/Controller/GuideController.php
@@ -1,9 +1,14 @@
 <?php
 namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class GuideController {
-    public function accueil() {
-        return $this->render('guideMichelin/accueil.html.twig');
+class GuideController extends AbstractController{
+    public function accueil($nom) {
+        return $this->render('guideMichelin/accueil.html.twig', array('nom' => $nom));
+    }
+
+    public function menu() {
+        return $this->render('guideMichelin/menu.html.twig');
     }
 }
