88cdb602491688047c9d5cf2b327c0343a5df493
Paul D <paul.duplessi@hotmail.fr>
2021-11-15 17:23:08 +0100

    update .env update accueil.html.twig add ajouter.html.twig update menu.html.twig update routes.yaml update guideMichelin/routes.yaml add voir.html.twig update GuideController.php

--- src/Controller/GuideController.php
+++ src/Controller/GuideController.php
@@ -2,13 +2,32 @@
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Resto;
 
 class GuideController extends AbstractController{
     public function accueil($nom) {
         return $this->render('guideMichelin/accueil.html.twig', array('nom' => $nom));
     }
 
-    public function menu() {
-        return $this->render('guideMichelin/menu.html.twig');
+    public function menu($nom) {
+        return $this->render('guideMichelin/menu.html.twig', array('nom' => $nom));
+    }
+
+    public function ajouter($nom,$chef,$etoiles){
+        $entityManager = $this->getDoctrine()->getManager();
+        $resto = new Resto;
+        $resto->setNom($nom);
+        $resto->setChef($chef);
+        $resto->setEtoiles($etoiles);
+        $entityManager->persist($resto);
+        $entityManager->flush();
+        return $this->render('guideMichelin/ajouter.html.twig', array('resto' => $resto));
+    }
+
+    public function voir($id) {
+        $resto = $this->getDoctrine()->getRepository(Resto::class)->find($id);
+        if(!$resto)
+            throw $this->createNotFoundException('Resto[id='.$id.'] inexistante');
+        return $this->render('guideMichelin/voir.html.twig', array('resto' => $resto));
     }
 }
